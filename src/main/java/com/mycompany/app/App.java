bfa116898b33f311fdd9bf72bca967111227c788
Christopher Lee <christopherlee12709@gmail.com>
2024-02-21 09:56:59 +0800

    changed to i am chris

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("I love fooooddd");
+        System.out.println("I am chriss");
     }
 
     public String getMessage() {
