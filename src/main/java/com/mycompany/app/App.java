2228cf1e05cafb751aa2b2ea8f873f4cb537e3ee
Christopher Lee <christopherlee12709@gmail.com>
2024-02-21 09:32:41 +0800

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Hi Im Chris");
+        System.out.println("I love keysight more than jenkins");
     }
 
     public String getMessage() {
