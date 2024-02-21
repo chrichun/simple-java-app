5e67a33419f808ceb52e440a6c16bca2d77eebeb
Christopher Lee <christopherlee12709@gmail.com>
2024-02-21 09:59:39 +0800

    changed to testing

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("I am chriss");
+        System.out.println("testing");
     }
 
     public String getMessage() {
